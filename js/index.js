7256223cd4cea42230897569d826779ea9deeb4e
Dean <dxsudbury@gmail.com>
2015-11-14 23:13:09 -0500

    v1.0.1
    Added blur themes and fixed a few bugs.

--- js/index.js
+++ js/index.js
@@ -1,9 +1,31 @@
 $(document).ready(function ($){
-	$("#jC-container").jCategorizer({
-	width: '55px',
-	height: '55px',
-	fontColor: '#000000',
-	border: '1px solid #969696',
+	$("#jC-container").jCategorizer();
+	
+	$("#jC-container2").jCategorizer({
+	width: '65px',
+	height: '65px'
+	});
+	
+	$("#jC-container3").jCategorizer({
+	width: '65px',
+	height: '65px',
+	iconPerRow: '2'
+	});
+	
+	$("#jC-container4").jCategorizer({
+	overlayType: 'blur',
+	width: '65px',
+	height: '65px',
+	iconPerRow: '2'
+	});
+	
+	$("#jC-container5").jCategorizer({
+	overlayType: 'blur',
+	width: '65px',
+	height: '65px',
+	background: '#ffb3b3',
+	fontColor: '#FF0000',
+	border: '2px dotted #FF0000',
 	iconMargin: '3px',
 	iconPerRow: '2'
 	});
--- js/jCategorizer.js
+++ js/jCategorizer.js
@@ -1,31 +1,86 @@
-;(function($) {
+/*!
+ * jQuery jCategorizer Plugin v1.0.1
+ * https://github.com/dean-xu/jCategorizer
+ *
+ * Copyright 2015 Dean Xu
+ * Released under the MIT license
+ */
+ 
+ ;(function($) {
 	$.fn.jCategorizer = function( options) {
 		var settings = $.extend({
+				overlayType  : 'shadow',
 				width        : '55px',
-				height        : '55px',
+				height       : '55px',
 				border       : '1px solid #969696',
 				background   : '#5f5f5f',
 				fontColor	 : '#000000',
 				iconPerRow   : '4',
 				iconMargin   : '1px'
 			}, options),
-			restoreStyle = "",
 		    setCategoryDesc = function ( elem, type ) {
 				elem.append( "<span class='jC-"+type+"Desc'>" + elem.attr("jc-desc") + "</span>" );
 			},
 			expandCategBox = function() {
-				restoreStyle = $(this).children(".jc-categBox").attr("style");
+				
 				$(this).addClass("jC-categPopup-active");
-				$("#jC-overlay").css("display","block");	
+				$(this).css("background",settings.background);
+				$(".jC-categPopup-active > div").css("top",window.pageYOffset + window.innerHeight*0.3 + "px");
+				if(settings.overlayType === "blur")
+				{
+					$(".jC-categPopup-container").css("display","block");
+					$(this).children('div').appendTo('.jC-categPopup-container');
+					$('body > *:not(".jC-categPopup-container")').addClass("jC-blur");	
+					$(".jC-overlay-blur").css("display","block");
+				}
+				else
+					$(".jC-overlay-shadow").css("display","block");
+				
 			},
 			collapsCategBox = function() {
-				$("#jC-overlay").css("display","none");
-				$(".jC-categPopup-active > .jc-categBox").attr("style",restoreStyle);
+				$(".jC-categPopup-active").css("background","");
+				if(settings.overlayType === "blur")
+				{
+					$(".jC-categPopup-container").css("display","none");
+					$('.jC-categPopup-container').children('div').appendTo('.jC-categPopup-active');
+					$('body > *:not(".jC-categPopup-container")').removeClass("jC-blur");
+					$(".jC-overlay-blur").css("display","none");
+				}
+				else
+					$(".jC-overlay-shadow").css("display","none");
+					
+				$(".jC-categPopup-active > div").css("top",$(".jC-categPopup-active > div").attr("jc-top"));
+				
 				$(".jC-categPopup-active").removeClass("jC-categPopup-active");
+			},
+			createOverlay = function(myoverlayType) {
+				if(myoverlayType === "blur")
+				{
+					if($(".jC-categPopup-container").length === 0)
+					{
+						$("body").append("<div class='jC-categPopup-container' style='display:none;'></div>");
+						$(".jC-categPopup-container").css("width","100%").css("height","100%");	
+					}
+					
+					if($(".jC-overlay-blur").length === 0)
+					{
+						$("body").append("<div class='jC-overlay jC-overlay-blur' style='display:none;'></div>");
+						$(".jC-overlay-blur").css("width","100%").css("height","100%");
+						$(".jC-overlay-blur").bind("click",collapsCategBox);		
+					}
+				}
+				else
+				{
+					if($(".jC-overlay-shadow").length === 0)
+					{
+						$("body").append("<div class='jC-overlay jC-overlay-shadow' style='display:none;'></div>");
+						$(".jC-overlay-shadow").css("width","100%").css("height","100%");
+						$(".jC-overlay-shadow").bind("click",collapsCategBox);		
+					}	
+				}				
+
 			};
-		$(this).append("<div id='jC-overlay' style='display:none;'></div>");
-		$("#jC-overlay").bind("click",collapsCategBox);
-		//collapsCategBox();
+		$(this).addClass("jC-container");
 		
         this.each( function() {
 		
@@ -41,34 +96,42 @@
 			$(this).children("ul").children("li").each( function() {
 				$(this).html("<div class='jc-categBox'>"+$(this).html()+"</div>");
 			});
-			
 			if ( settings.fontColor )
 				$(this).css( 'color', settings.fontColor );
             if ( settings.width )
 			{
 				$(this).children("ul").children("li").css( 'width', settings.width );
-				$(".jc-categBox").css( 'width', settings.width );
+				$(this).find(".jc-categBox").css( 'width', settings.width );
 			}
 			if ( settings.height )
 			{
 				$(this).children("ul").children("li").css( 'height', settings.height );
-				$(".jc-categBox").css( 'height', settings.height );
+				$(this).find(".jc-categBox").css( 'height', settings.height );
 			}
 			if ( settings.border )
-				$(this).children("ul").children("li").css( 'border', settings.border );
+			{
+				$(this).children("ul").children("li").children(".jc-categBox").css( 'border', settings.border );
+			}
 			if ( settings.background)
-				$(this).children("ul").children("li").css("background", settings.background);
+			{
+				$(this).children("ul").children("li").children(".jc-categBox").css("background", settings.background);
+			}
 			if ( settings.iconPerRow)
 			{
-				$(".jc-categBox img").css("width", ((parseInt(settings.width.replace("px","")))/parseInt(settings.iconPerRow) -  2*parseInt(settings.iconMargin ) - 0.5).toString() + "px");
-				$(".jc-categBox img").css("height", ((parseInt(settings.width.replace("px","")))/parseInt(settings.iconPerRow) -  2*parseInt(settings.iconMargin ) - 0.5).toString() + "px");
+				$(this).find(".jc-categBox img").css("width", ((parseInt(settings.width.replace("px","")))/parseInt(settings.iconPerRow) -  2*parseInt(settings.iconMargin ) - 0.5).toString() + "px");
+				$(this).find(".jc-categBox img").css("height", ((parseInt(settings.width.replace("px","")))/parseInt(settings.iconPerRow) -  2*parseInt(settings.iconMargin ) - 0.5).toString() + "px");
 			}
 			if ( settings.iconMargin)
-				$(".jc-categBox > ul > li").css("margin", settings.iconMargin);
+				$(this).find(".jc-categBox > ul > li").css("margin", settings.iconMargin);
 			
+			if (settings.overlayType)
+				createOverlay(settings.overlayType);
+				
 			$(this).children("ul").children("li").children(".jc-categBox").each( function() {
-				$(this).css("left",$(this).position().left+"px");
-				$(this).css("top",$(this).position().top+"px");
+				$(this).attr("jc-left",$(this).position().left+"px");
+				$(this).attr("jc-top",$(this).position().top+"px");
+				$(this).css("left",$(this).attr("jc-left"));
+				$(this).css("top",$(this).attr("jc-top"));
 			});
 
         });
